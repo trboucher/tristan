f46a5e1aa1a1a9abffb871d1be965a9c1cde0a5c
trboucher <95898394+trboucher@users.noreply.github.com>
2021-12-12 20:51:13 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function reveal1() {
+  document.getElementById("HTMLcheatSheet").style.display = 'block';
+  document.getElementById("viewButton1").style.display = 'none';
+}
+
+function reveal2() {
+  document.getElementById("CSScheatSheet").style.display = 'block';
+  document.getElementById("viewButton2").style.display = 'none';
+}
